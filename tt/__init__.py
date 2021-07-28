bd35d7128c586b7af2893e9babd5bfbe85332395
AlexApps99 <alex.apps99@gmail.com>
2021-07-28 22:52:48 +1200

    Bruh

--- tt/__init__.py
+++ tt/__init__.py
@@ -9,17 +9,22 @@ from .index import idx_page
 def create_app():
     app = Flask(__name__, static_url_path='')
     app.secret_key = b'}\xdc\x04\x9e\xba\xa8\xda0,@\x8e\x03\x9a[\xec\xb9'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+    if app.env == "production":
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tt.db'
+    else:
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     login_manager.init_app(app)
     db.init_app(app)
     app.register_blueprint(map_page, url_prefix="/map")
     app.register_blueprint(idx_page)
     #app.context_processor(i18n_processor)
-    setup_db(app)
+    if app.env != "production":
+        setup_db(app)
     return app
 
 
+# please consider maori it will up our grade a shitton
 def i18n_processor():
     def i18n(s):
         return s
@@ -33,33 +38,34 @@ def setup_db(app):
         me = User(email="alex.apps99@gmail.com",
                   password_hash=hashp("GeffIsGreat123"),
                   godmode=True)
-        marker = Marker(name="Northcote College",
-                        description="Testing Time HQ",
-                        lat=-36.8095,
-                        lon=174.7338)
-        # make the current date be the default
-        test = Test(
-            date=datetime.now(),
-            nitrate=0,
-            nitride=0,
-            ph=6,
-            free_cl=0,
-            total_cl=0,
-            hardness=0,
-        )
-        prevtest = Test(
-            date=datetime.now() - timedelta(days=3),
-            nitrate=0,
-            nitride=0,
-            ph=4,
-            free_cl=0,
-            total_cl=0,
-            hardness=0,
-        )
+        if app.env != "production":
+            marker = Marker(name="Northcote College",
+                            description="Testing Time HQ",
+                            lat=-36.8095,
+                            lon=174.7338)
+            # make the current date be the default
+            test = Test(
+                date=datetime.now(),
+                nitrate=0,
+                nitride=0,
+                ph=6,
+                free_cl=0,
+                total_cl=0,
+                hardness=0,
+            )
+            prevtest = Test(
+                date=datetime.now() - timedelta(days=3),
+                nitrate=0,
+                nitride=0,
+                ph=4,
+                free_cl=0,
+                total_cl=0,
+                hardness=0,
+            )
 
-        marker.owner = me
-        prevtest.marker = marker
-        test.marker = marker
+            marker.owner = me
+            prevtest.marker = marker
+            test.marker = marker
         db.session.add(me)
         db.session.commit()
         # print(
